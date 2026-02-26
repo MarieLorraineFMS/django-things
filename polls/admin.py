d8c2fa083266223fdb7402b592e078f5d37cdde7
Marie-Lorraine Collignon <marie-lorraine.collignon@fms-ea.com>
2026-02-26 15:23:42 +0100

    4.1 Admin style

--- polls/admin.py
+++ polls/admin.py
@@ -4,17 +4,30 @@ Configuration de l'admin dashboard.
 from django.contrib import admin
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 # 3 lignes vides pour ajouter des choix rapidement
+
 class QuestionAdmin(admin.ModelAdmin):
     """
     Personnalise la façon dont les questions sont affichées dans l'admin.
     """
-    # 4.1 : colonnes
+    # 4.1 : on regroupe par thématique
+    fieldsets = [
+        ("Contenu principal", {"fields": ["question_text"]}),
+        ("Calendrier de publication", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+
+    # Insertion des choix dans le form directement
+    inlines = [ChoiceInline]
+
+
+    # 4.1 : Personalisation de la liste
     list_display = ('question_text', 'pub_date', 'display_was_published_recently')
 
     # 4.1 :
-    # On transforme un "true/false" en une icône : Check vert | Croix rouge
+    # Transforme un "true/false" en icône : Check vert | Croix rouge
     # Plus visuel & rapide à comprendre
-
     @admin.display(boolean=True, description='Publié récemment ?')
     def display_was_published_recently(self, obj: Question) -> bool:
         return obj.was_published_recently()
@@ -32,4 +45,12 @@ class ChoiceAdmin(admin.ModelAdmin):
 # 4.1 : enregistrement des modèles
 # On dit à Django : "Utilise ces modèles avec ces réglages de tableau de bord"
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
+
+# Onglet du nav
+admin.site.site_title = "Admin Sondages"
+
+# Navbar text
+admin.site.site_header = "TABLEAU DE BORD"
+
+# Accueil titre
+admin.site.index_title = "Ouelcome, on casse quoi aujourd'hui ?"
